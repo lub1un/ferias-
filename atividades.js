7711cb2bc365bee67c93ae5f741c07b0ead3d697
lub1un <bmarcusvinicius930@gmail.com>
2024-07-23 20:22:35 -0300

    update:

---
+++ atividades.js
@@ -0,0 +1,65 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+const  atividades = [];
+
+const modelo = () => {
+    let atividade = {
+        dia: undefined,
+        descricao: [],
+    }
+    atividade.dia = prompt("Em qual dia? ");
+
+    while (true) {
+        let resposta = prompt("O que fez nesse dia? ");
+
+        if (resposta == "acabou") break;
+
+        atividade.descricao.push(resposta);
+
+    }
+    return atividade;
+}
+
+const criar = () => {
+    let atividade= modelo();
+
+    atividades.push(atividade);
+
+    console.log("Atividade criada");
+};
+
+const listar = () => {
+    atividades.forEach((atividade, indice) => {
+        console.log(`${indice + 1}. ${atividade.dia}: `);
+        atividade.descricao.forEach((descricao) => console.log(`- ${descricao}`));
+    });
+};
+
+const atualizar = () => {
+    listar();
+
+    let indice = prompt("Qual indice deseja atualizar? ");
+
+    let atividade = modelo()
+
+    atividades[--indice] = atividade;
+
+    console.log("Atualizado");
+};
+
+const remover = () => {
+    listar()
+
+    let indice = prompt("Qual indice sera removido? ");
+
+    atividades.splice(--indice, 1);
+
+    console.log("Removido");
+};
+
+module.exports = {
+    criar,
+    atualizar,
+    remover,
+    listar,
+};
